e5b1fd5c2e0d1cbb618e97447ca056a52be7676c
PamelaZelaya <90662975+PamelaZelaya@users.noreply.github.com>
2023-09-23 00:14:13 -0600

    Hola Diosito

--- src/app.js
+++ src/app.js
@@ -14,11 +14,30 @@ app.use(bodyParser.json())
 app.use(express.json())
 app.use(morgan('dev'))
 
-app.use('/api',router)
+app
 
 
-//app.listen(PORT,()=>console.log(`Servidor corriendo en puerto ${PORT}`)) 
+// import express  from "express";
+// import bodyParser from "body-parser"
+// import router from "./routes/rutas.js";
+// import cors from "cors";
+// import morgan from 'morgan'//desinstalable
 
-app.listen(app.get('port'), '0.0.0.0', () => {
-    console.log(`Server listening on port ${app.get('port')}`);
-  });
+// const app = express();
+// app.set('port', process.env.PORT || 4000);
+// const PORT = process.env.PORT || 4000;
+// app.use(express.json());
+
+// app.use(cors())
+// app.use(bodyParser.json())
+// app.use(express.json())
+// app.use(morgan('dev'))
+
+// app.use('/api',router)
+
+
+// app.listen(PORT,()=>console.log(`Servidor corriendo en puerto ${PORT}`)) 
+
+// app.listen(app.get('port'), '0.0.0.0', () => {
+//     console.log(`Server listening on port ${app.get('port')}`);
+//   });
