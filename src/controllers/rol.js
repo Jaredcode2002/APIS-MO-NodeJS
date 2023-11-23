72edbf87c0ec7d18db1fa84ab2d24cac8248dd14
EAMP2000 <erickpavon544@gmail.com>
2023-11-23 14:27:30 -0600

    Validaciones de no duplicados en todos los mantenimientos

--- src/controllers/rol.js
+++ src/controllers/rol.js
@@ -12,10 +12,15 @@ export const ContrRol = {
     },
 
     postRol: async (req, res) => {
+        const { Rol, Descripcion, estado} = req.body;
         try {
-            const { Rol, Descripcion, estado} = req.body;
             const result = await ModRol.postRol({ Rol, Descripcion, estado});
-            res.status(201).json({ id: result.id });
+            if (result == false) {
+                res.status(201).json(result);
+              } else {
+                res.status(201).json(result);
+              }
+
         } catch (error) {
             console.log(error);
         }
