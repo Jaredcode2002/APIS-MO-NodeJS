a9a89dbf53d08fee5c4a5bcfe4ea365bb9264c75
EAMP2000 <erickpavon544@gmail.com>
2023-11-26 00:19:41 -0600

    Quite validaciones que fallaron en upd duplicados

--- src/controllers/genero.js
+++ src/controllers/genero.js
@@ -1,56 +1,53 @@
 import { ModGenero } from "../models/genero.js";
 
-export const ContrGenero = {
+export const ContrGenero= {
 
-    getGenero: async (req, res) => {
-        const genero = await ModGenero.getGenero()
+    getGenero:async(req,res)=>{
+        const genero =await ModGenero.getGenero()
         res.json(genero)
     },
 
-    getGeneroInactivos: async (req, res) => {
-        const genero = await ModGenero.getGeneroInactivos()
+    getGeneroInactivos:async(req,res)=>{
+        const genero =await ModGenero.getGeneroInactivos()
         res.json(genero)
     },
 
-    postInsertGenero: async (req, res) => {
+    postInsertGenero:async(req,res)=>
+    {
         try {
-            const { descripcion, estado } = req.body;
-            const result = await ModGenero.postInsertGenero({ descripcion, estado });
+            const  {descripcion, estado}=req.body;
+            const result = await ModGenero.postInsertGenero({descripcion,estado});
             if (result == false) {
                 res.status(201).json(result);
-            } else {
+              } else {
                 res.status(201).json(result);
+              }
+            } catch (error) {
+              console.log(error);
             }
-        } catch (error) {
-            console.log(error);
-        }
     },
-
-    putUpdateGenero: async (req, res) => {
+        
+    putUpdateGenero : async (req,res)=>{
         try {
-            const { descripcion, estado, IdGenero } = req.body;
-            const result = await ModGenero.putUpdateGenero({ descripcion, estado, IdGenero });
-            if (result == false) {
-                res.status(201).json(result);
-            } else {
-                res.status(201).json(result);
-            }
+            const{descripcion, estado, IdGenero} = req.body;
+            const result  = await ModGenero.putUpdateGenero({descripcion, estado, IdGenero});
+            res.status(201).json({ id: result.id});
         } catch (error) {
             console.log(error);
         }
     },
-
-    deleteGenero: async (req, res) => {
+        
+            deleteGenero : async (req,res)=>{
         try {
-            const { IdGenero } = req.body;
-            const result = await ModGenero.deleteGenero({ IdGenero });
-            res.status(201).json({ id: result.id });
-
+            const{IdGenero} = req.body;
+            const result  = await ModGenero.deleteGenero({IdGenero});
+           res.status(201).json({ id: result.id});
+            
         } catch (error) {
             console.log(error);
         }
-
-    },
-
+        
+        },
+         
 
 }
