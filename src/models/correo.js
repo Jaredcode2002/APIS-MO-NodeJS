28d9b33fcc4b85553f8518f6c44ff5275a7c41ef
SLara26 <seydi.lara@unah.hn>
2023-11-28 23:58:13 -0600

    cambios

--- src/models/correo.js
+++ src/models/correo.js
@@ -1,26 +1,54 @@
 import nodemailer from "nodemailer";
+import { connectDB } from "../config/Conn.js";
 
 const enviarMail = async (mail) => {
-  const config = {
-    host: "smtp.gmail.com",
-    port: 587,
-    auth: {
-      user: "multiopticashn504@gmail.com",
-      pass: "hlnc bxyg ahpg ngfs",
-    },
-  };
+  let conexion;
+  try {
+    conexion = await connectDB();
 
-  const mensaje = {
-    from: "multiopticashn504@gmail.com",
-    to: mail.para,
-    subject: mail.titulo,
-    text: mail.texto,
-  };
+    const [filaCorreo] = await conexion.query("SELECT valor FROM tbl_ms_parametros where Id_Parametro=3;");
+    // host = filasHost[0].valor;
+    console.log(filaCorreo[0].valor);
 
-  const transport = nodemailer.createTransport(config);
-  const info = await transport.sendMail(mensaje);
+    const [filaCorreoPass] = await conexion.query("SELECT valor FROM tbl_ms_parametros where Id_Parametro=15;");
+    // host = filasHost[0].valor;
+    console.log(filaCorreoPass[0].valor);
+
+
+
+    const config = {
+      host: "smtp.gmail.com",
+      port: 587,
+      auth: {
+        user: filaCorreo[0].valor,
+        pass: filaCorreoPass[0].valor,
+      },
+    };
+
+
+    //3
+
+
+    const mensaje = {
+      from: filaCorreo[0].valor,
+      to: mail.para,
+      subject: mail.titulo,
+      text: mail.texto,
+    };
+
+    const transport = nodemailer.createTransport(config);
+    const info = await transport.sendMail(mensaje);
+  } catch (error) {
+    throw error; // Propaga el error
+  } finally {
+    if (conexion) {
+      conexion.end(); // Cierra la conexión en cualquier caso
+    }
+  }
+
+
 };
 
 export const MailMod = {
-    sendMail: enviarMail,
-  };
+  sendMail: enviarMail,
+};
