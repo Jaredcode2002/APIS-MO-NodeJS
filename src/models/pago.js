1955d4e63e0b116138b9bb79003302a179d8be56
SLara26 <seydi.lara@unah.hn>
2023-12-03 12:57:18 -0600

    cambios

--- src/models/pago.js
+++ src/models/pago.js
@@ -30,7 +30,7 @@ export const ModPago = {
         conexion.end()
         return {estado:"Pagado"}
       }else{
-        const [pagos]= await conexion.query("SELECT CONCAT(cli.nombre,' ',cli.apellido) as cliente,tp.descripcion,p.fecha,p.`saldoAbono`,p.`saldoRestante`  FROM tbl_pago as p inner join tbl_venta as v on p.`IdVenta`=v.`IdVenta` inner join tbl_cliente as cli on v.`IdCliente`=cli.`idCliente` inner join tbl_tipopago as tp on p.`IdTipoPago`=tp.`IdTipoPago`")
+        const [pagos]= await conexion.query("SELECT CONCAT(cli.nombre,' ',cli.apellido) as cliente,tp.descripcion,p.fecha,p.saldoAbono,p.saldoRestante  FROM tbl_pago as p inner join tbl_venta as v on p.IdVenta=v.IdVenta inner join tbl_cliente as cli on v.IdCliente=cli.idCliente inner join tbl_tipopago as tp on p.IdTipoPago=tp.IdTipoPago")
         conexion.end()
         return pagos
       }
